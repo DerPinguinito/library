e698b81ff16936b020b93ebc2e769c7911d363f4
DerPinguinito <176357385+DerPinguinito@users.noreply.github.com>
2024-11-21 23:42:36 -0300

    Adds lib method to loop through the library & changes naming convention
    The new method prints the books stored in library to the console.
    Book "names" where changed to "title" for more accuracy.

--- assets/scripts/book.js
+++ assets/scripts/book.js
@@ -1,5 +1,5 @@
-export default function Book(name, author, year, pages, read = false) {
-    this.name = name;
+export default function Book(title, author, year, pages, read = false) {
+    this.title = title;
     this.author = author;
     this.year = year;
     this.pages = pages;
--- assets/scripts/lib.js
+++ assets/scripts/lib.js
@@ -4,16 +4,36 @@ import Book from './book.js'
 const LIBRARY = []
 
 const lib = {
-    setBook(name, author, year, pages, read) {
-        const BOOK = new Book(name, author, year, pages, read);
+    setBook(title, author, year, pages, read) {
+        const BOOK = new Book(title, author, year, pages, read);
         return BOOK;
     },
-    addBookToLibrary(book) {;
+    addBookToLibrary(book) {
         LIBRARY.push(book);
     },
     printLibrary() {
         console.log(LIBRARY);
+    },
+    loopThroughLibrary() {
+        LIBRARY.forEach(book => {
+            console.log(`${book.title} was written by ${book.author} in ${book.year}. It is ${book.pages} pages long.`);
+        })
     }
 }
+
+function entryPoint() {
+    const a = lib.setBook("book1", "author1", 1001, 100, true);
+    const b = lib.setBook("book2", "author1", 1001, 100, true);
+    lib.addBookToLibrary(a);
+    lib.addBookToLibrary(b);
+    lib.loopThroughLibrary();
+}
+
+function run() {
+    entryPoint();
+}
+
+run();
+
 
 export default lib;
